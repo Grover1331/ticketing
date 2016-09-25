e9b7fa614f0f15f24a89405f59048b060af189fd
Grover1331 <grover.abhinav82@gmail.com>
2016-09-25 17:14:59 +0530

    checking body in alert

--- app_server/controllers/restaurants.js
+++ app_server/controllers/restaurants.js
@@ -32,7 +32,7 @@ module.exports.homeRestaurantList = function(req, res) {
   request(
     requestOptions,
     function(err, response, body) {
-		res.send(500,'errorMessage');
+		res.send(500,body);
       renderHomepage(req, res, body);
     }
   );
